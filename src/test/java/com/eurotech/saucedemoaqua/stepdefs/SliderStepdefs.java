5fe0fe6cac85c5d66e64f1736bf1978faa391d94
GulcanAkuklu <gulcanakuklu3@gmail.com>
2023-05-05 19:34:35 +0200

    slider feature added

--- src/test/java/com/eurotech/saucedemoaqua/stepdefs/SliderStepdefs.java
+++ src/test/java/com/eurotech/saucedemoaqua/stepdefs/SliderStepdefs.java
@@ -4,6 +4,7 @@ import com.eurotech.saucedemoaqua.pages.DemoqaSliderPage;
 import com.eurotech.saucedemoaqua.utilities.ConfigurationReader;
 import com.eurotech.saucedemoaqua.utilities.Driver;
 import io.cucumber.java.en.*;
+import org.junit.Assert;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
@@ -15,25 +16,32 @@ public class SliderStepdefs {
     @Given("the user is on the slider page")
     public void the_user_is_on_the_slider_page() {
         Driver.get().get(ConfigurationReader.get("url1"));
+        Assert.assertTrue(demoqaSliderPage.inputSliderValue2.isDisplayed());
 
     }
     @When("the user moves the slider to {int}")
-    public void the_user_moves_the_slider_to(Integer targetSliderRange) {
+    public void the_user_moves_the_slider_to(int targetSliderRange) {
 
         // 1.way
-        Actions move = new Actions(Driver.get());
-         move.dragAndDropBy(demoqaSliderPage.inputSliderValue2, 10, 0).perform();
-
+//        Actions move = new Actions(Driver.get());
+//       move.moveToElement(demoqaSliderPage.inputSliderValue2, 50, 0).perform();
         //2.way
-//        for (int i = 1; i <= 50 ; i++) {
-//            demoqaSliderPage.inputSliderValue2.sendKeys(Keys.ARROW_RIGHT);
-//        }
 
+        int initialSliderRange= Integer.parseInt(demoqaSliderPage.inputSliderValue2.getAttribute("value"));
+        int numberOfTimesSliderToMove=targetSliderRange-initialSliderRange;
+        for (int i = 1; i <= numberOfTimesSliderToMove ; i++) {
+            demoqaSliderPage.inputSliderValue2.sendKeys(Keys.ARROW_RIGHT);
+        }
         System.out.println("slider value = " + demoqaSliderPage.inputSliderValue.getAttribute("value"));
+
 
     }
     @Then("the slider should be ranged at {int}")
-    public void the_slider_should_be_ranged_at(Integer int1) {
+    public void the_slider_should_be_ranged_at(int expectedSliderRange) {
+        int actualSliderRange=Integer.parseInt(demoqaSliderPage.inputSliderValue2.getAttribute("value"));
+        Assert.assertEquals(expectedSliderRange,actualSliderRange);
+
+
 
     }
 }
